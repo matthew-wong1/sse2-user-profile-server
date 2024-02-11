621e80f62dd277f32624157ba67ecd5eb16a27c1
Matthew Wong <matthew.wong1@outlook.com>
2024-02-11 23:52:53 +0000

    add basic user profile page

---
+++ client/src/Components/UserProfile.js
@@ -0,0 +1,18 @@
+// UserInfo.js
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+function UserInfo() {
+//   const location = useLocation();
+//   const { user } = location.state;
+
+  return (
+    <div>
+      <h1>User Information</h1>
+      {/* <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p> */}
+    </div>
+  );
+}
+
+export default UserInfo;
