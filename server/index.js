cfb130f54a9ce8ae1f0158d98de59d97ba664a8d
Matthew Wong <matthew.wong1@outlook.com>
2024-02-11 23:29:58 +0000

    add outline of api endpoints

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
+// FOR DEVELOPMENT PURPOSES
 const IP_ADDRESS = '127.0.0.1';
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// FOR DEVELOPMENT PURPOSES
 app.use(cors());
 
 app.use((req, res, next) => {
@@ -15,6 +17,28 @@ app.use((req, res, next) => {
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+// Get user profile information
+app.get("/api/user", (req, res) => {
+  // Authenticate JWT (another microservice?)
+
+  // If authentication fails, throw error (show error page with correct http status)
+
+  // Check cache. If has user details, return 
+
+  // Else, execute SQL queries 
+
+  // Cache 
+
+  // Return JSON of user 
+
+});
+
+// User signup 
+app.post('/api/signup', (req, res) => {
+  // Form validation 
+
 });
 
 app.listen(PORT, IP_ADDRESS, () => {
