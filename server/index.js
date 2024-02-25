785748c30d86c696fa8bc6b44b823aefba6c575a
Matthew Wong <matthew.wong1@outlook.com>
2024-02-25 16:52:53 +0000

    make container refresh on push

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/api", (req, res) => {
-	res.json({ message: "Hello from server!" });
+	res.json({ message: "Hello from server again!" });
 });
 
 const getDegrees = async () => {
