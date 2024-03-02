5d930683b6a145852568e28dc13067a2f160d335
Matthew Wong <matthew.wong1@outlook.com>
2024-03-02 17:08:13 +0000

    modify dockerfile

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,4 @@ COPY . .
 EXPOSE 3000
 
 # Command to run the server
-CMD ["node", "app.js"]
+CMD ["node", "lambda.js"]
--- routers/login.js
+++ routers/login.js
@@ -5,71 +5,71 @@ const jwt = require("jsonwebtoken");
 const { pool } = require("../controllers/pools");
 
 Router.post("/", async (request, response, next) => {
-  try {
-    const { username, password } = request.body;
+	try {
+		const { username, password } = request.body;
 
-    var passwordRetrieved, uni;
+		var passwordRetrieved, uni;
 
-    // const pool = await pools(consoPool);
-    // const pool = mysql.createPool({
-    //   host: process.env.MYSQL_HOST,
-    //   user: process.env.MYSQL_USER,
-    //   password: process.env.MYSQL_PASSWORD,
-    //   database: process.env.MYSQL_DATABASE,
-    //   port: process.env.MYSQL_PORT
-    // });
+		// const pool = await pools(consoPool);
+		// const pool = mysql.createPool({
+		//   host: process.env.MYSQL_HOST,
+		//   user: process.env.MYSQL_USER,
+		//   password: process.env.MYSQL_PASSWORD,
+		//   database: process.env.MYSQL_DATABASE,
+		//   port: process.env.MYSQL_PORT
+		// });
 
-    const [rows] = await pool.query(
-      `SELECT passwordhash, university FROM users a LEFT JOIN universities b 
-                                                     ON a.universityid = b.universityid 
-                                                     WHERE userid = ?
-                                                     `,
-      [username]
-    );
+		const [rows] = await pool.query(
+			`SELECT passwordhash, university FROM users a LEFT JOIN universities b 
+                                                      ON a.universityid = b.universityid 
+                                                      WHERE userid = ?
+                                                      `,
+			[username]
+		);
 
-    if (rows.length === 0) {
-      return response.status(401).send({ error: "User credentials are wrong" });
-    } else {
-      passwordRetrieved = rows[0].passwordhash;
-      uni = rows[0].university;
-    }
+		if (rows.length === 0) {
+			return response.status(401).send({ error: "User credentials are wrong" });
+		} else {
+			passwordRetrieved = rows[0].passwordhash;
+			uni = rows[0].university;
+		}
 
-    bcrypt.compare(password, passwordRetrieved, function (error, res) {
-      if (!res || error) {
-        return response
-          .status(401)
-          .send({ error: "User credentials are wrong" });
-      } else {
-        const token = jwt.sign(
-          { username: username, uni: uni },
-          process.env.SECRET
-        );
-        return response.status(200).send({token, uni});
-      }
-    });
-  } catch (error) {
-    response.status(401).send({ error: error.message });
-    next(error);
-  }
+		bcrypt.compare(password, passwordRetrieved, function (error, res) {
+			if (!res || error) {
+				return response
+					.status(401)
+					.send({ error: "User credentials are wrong" });
+			} else {
+				const token = jwt.sign(
+					{ username: username, uni: uni },
+					process.env.SECRET
+				);
+				return response.status(200).send({ token, uni });
+			}
+		});
+	} catch (error) {
+		response.status(401).send({ error: error.message });
+		next(error);
+	}
 });
 
 Router.get("/authenticate", async (request, response, next) => {
-  try {
-    const bearerHeader = request.headers["authorization"];
+	try {
+		const bearerHeader = request.headers["authorization"];
 
-    if (!bearerHeader) {
-      return response.status(401).send({ error: "No Bearer token provided" });
-    }
+		if (!bearerHeader) {
+			return response.status(401).send({ error: "No Bearer token provided" });
+		}
 
-    const bearerToken = bearerHeader.split(" ")[1];
-    const decodedToken = jwt.verify(bearerToken, process.env.SECRET);
-    return response
-      .status(200)
-      .send({ username: decodedToken.username, uni: decodedToken.uni });
-  } catch (error) {
-    response.status(401).send({ error: error.message });
-    next(error);
-  }
+		const bearerToken = bearerHeader.split(" ")[1];
+		const decodedToken = jwt.verify(bearerToken, process.env.SECRET);
+		return response
+			.status(200)
+			.send({ username: decodedToken.username, uni: decodedToken.uni });
+	} catch (error) {
+		response.status(401).send({ error: error.message });
+		next(error);
+	}
 });
 
 module.exports = Router;
